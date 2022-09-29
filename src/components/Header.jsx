a13836cc76de42f5fc53c5e556b7f0e2b4e4ec89
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-09-29 16:02:55 -0300

    1st prototype

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styled from "styled-components";
+import blueBall from "../../public/blueBall.svg";
 
 export default function Header() {
   return (
-    <nav>
-      <img src="https://placekitten.com/151/300" alt="*" />
+    <Nav>
+      <img src="https://placekitten.com/151/100" alt="*" />
       <NavContainer>
         <NavLinks>
           <a>Sobre</a>
@@ -16,33 +17,56 @@ export default function Header() {
           <a>Contato</a>
         </NavLinks>
       </NavContainer>
-    </nav>
+    </Nav>
   );
 }
 
-const NavContainer = styled.ul`
-  background-color: black;
-  position: fixed;
+const Nav = styled.nav`
   display: flex;
-  flex-direction: column;
   justify-content: space-around;
-  left: 0;
-  height: calc(100vh - 305px);
-  width: 150px;
-  border: 1px solid white;
-  box-shadow: 0 0 6px 0px lightblue;
+  align-items: center;
+`;
+const NavContainer = styled.ul`
+  display: flex;
+  flex-direction: row;
 `;
 const NavLinks = styled.li`
+  border-radius: 1rem;
+  padding: 10px;
+  margin-left: 1rem;
+  background-color: white;
   font-weight: bold;
   font-size: 1.2rem;
-  transform: rotate(-90deg);
   color: white;
-  padding: 1rem;
-  text-align: center;
+  border: 1px solid white;
+  box-shadow: 0 0 6px 0px white;
   :hover {
-    color: lightblue;
+    box-shadow: 0 0 10px 0px white;
+    animation: hologram 30s linear infinite;
   }
   a {
+    padding: 1rem 2rem;
+    color: black;
+    background-color: transparent;
     cursor: pointer;
+  }
+
+  @keyframes hologram {
+    0% {
+      background-image: repeating-linear-gradient(#0000ff 1%, #00ffff 12%);
+      background-position-y: 0px;
+      box-shadow: none;
+    }
+    .1% {
+      box-shadow: 0 0 20px 5px white;
+    }
+    .12% {
+      box-shadow: 0 0 6px 0px white;
+    }
+    /* 2% {
+    } */
+    100% {
+      background-position-y: 200px;
+    }
   }
 `;
