6baabf5814f6439c46c1e4a02aa3168ca378bee6
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-10-05 21:54:29 -0300

    basic style

--- src/components/2ndPart/About.jsx
+++ src/components/2ndPart/About.jsx
@@ -5,16 +5,21 @@ export default function About() {
   return (
     <AboutSection>
       <div>
-        <img src="" alt="uma pessoa bem bonita, mas conhecida como eu " />
+        <img
+          src="/perfil.png"
+          alt="uma pessoa bem bonita, mas conhecida como eu "
+        />
       </div>
-      <h2>Sobre mim</h2>
-      <p>
-        Meu nome é João Pedro, tenho 20 anos. Sou programador Front-end.
-        Atualmente busco uma oportunidade para adquirir experiência na área da
-        tecnologia com programação e desenvolvimento web. Adoro criar e dar vida
-        para minhas ideias mais ambiciosas, os desafios fazem parte da minha
-        rotina.
-      </p>
+      <div>
+        <h2>Sobre mim</h2>
+        <p>
+          Meu nome é João Pedro, tenho 20 anos. Sou programador Front-end.
+          Atualmente busco uma oportunidade para adquirir experiência na área da
+          tecnologia com programação e desenvolvimento web. Adoro criar e dar
+          vida para minhas ideias mais ambiciosas, os desafios fazem parte da
+          minha rotina.
+        </p>
+      </div>
     </AboutSection>
   );
 }
@@ -22,6 +27,14 @@ export default function About() {
 const AboutSection = styled.section`
   position: relative;
   margin-top: 26px;
+  font-family: "Dax";
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  img {
+    width: 400px;
+    height: 400px;
+  }
   h2 {
     font-size: 1rem;
   }
