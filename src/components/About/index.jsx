763e478f82a670d507e4bb27bd2f11606cf05d12
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-10-10 20:55:20 -0300

    factored icons map

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,27 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import {
-  SiHtml5,
-  SiCss3,
-  SiJavascript,
-  SiBootstrap,
-  SiReact,
-  SiNextdotjs,
-  SiStyledcomponents,
-  SiSass,
-} from "react-icons/si";
 import { cinzaClaro, begeBG } from "../../styles/UI/variaveis";
+import AboutIcons from "./AboutIcons";
 
 export default function About() {
-  const icons = [
-    SiHtml5,
-    SiJavascript,
-    SiReact,
-    SiNextdotjs,
-    SiBootstrap,
-    SiStyledcomponents,
-    SiSass,
-  ];
   return (
     <AboutSection>
       <ContentDiv>
@@ -48,21 +30,7 @@ export default function About() {
         </div>
       </ContentDiv>
       <h2>Ferramentas com que trabalho</h2>
-      <IconDiv>
-        {icons.map((Icon, i) => {
-          let names = Icon.name.slice(2);
-          if (names.charAt(0) === "N") {
-            names = "Next.js";
-          }
-
-          return (
-            <span key={i}>
-              <Icon />
-              <p href="/">{names}</p>
-            </span>
-          );
-        })}
-      </IconDiv>
+      <AboutIcons />
     </AboutSection>
   );
 }
@@ -124,32 +92,6 @@ const BotaoResumo = styled.button`
     transition: 0.1s ease-in-out;
     color: black;
     background-color: rgb(198, 201, 156);
-  }
-`;
-
-const IconDiv = styled.div`
-  align-items: center;
-  flex-wrap: wrap;
-  display: flex;
-  text-align: center;
-  gap: 1rem;
-  svg {
-    font-size: 4rem;
-  }
-  p {
-    font-size: 1rem;
-    font-weight: bold;
-  }
-  span {
-    padding: 2% 0;
-    background-color: antiquewhite;
-    border-radius: 15px;
-    min-width: 148px;
-    border: 2px solid lightgrey;
-  }
-  span:hover {
-    transition: 0.1s ease-in-out;
-    background-color: white;
   }
 `;
 
