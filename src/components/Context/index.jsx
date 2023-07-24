a616ef4f53bbe32e9964d1d2be3b43feb013c3a9
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2023-07-23 22:12:29 -0300

    Confidencial button context

--- src/components/Context/index.jsx
+++ src/components/Context/index.jsx
@@ -4,12 +4,15 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [isPtbr, setIsPtbr] = useState(true);
+  const [confidencial, setConfidencial] = useState(false);
   const toggleLang = (is) => {
     setIsPtbr(is);
     console.log(isPtbr ? "Português brasileiro" : "English");
   };
   return (
-    <AppContext.Provider value={{ isPtbr, toggleLang }}>
+    <AppContext.Provider
+      value={{ isPtbr, toggleLang, confidencial, setConfidencial }}
+    >
       {children}
     </AppContext.Provider>
   );
