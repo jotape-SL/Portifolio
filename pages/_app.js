eded88f6f8c88e00d6fc5da20139e8d856510e32
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2023-08-09 23:34:25 -0300

    Font fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import { AppProvider } from "../src/components/Context";
 import GlobalStyle from "../src/styles/globals";
 import FontStyles from "../src/styles/UI/fontes";
 
+cosnt 
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
